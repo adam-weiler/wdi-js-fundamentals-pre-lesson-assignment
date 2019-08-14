fa22b8275961f2e88eb48c62a7c4d194acd85e0e
adam-weiler <adam.weiler@yahoo.ca>
2019-08-14 17:14:22 -0400

    There are 4 ghosts stored in the array. These are their names.

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -1,1 +1,8 @@
+const ghost = "Blinky!"; //This variable assignment is not a comment, but everything after the // in this sentence is a comment.
+
+/* 
+These lines are 
+also comments.
+*/
+
 console.log('Pac-Man eats ghosts for lunch.');
