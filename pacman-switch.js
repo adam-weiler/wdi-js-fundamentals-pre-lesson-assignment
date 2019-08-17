a98d10db8b54d0266973ee6e52714ee8d7bdd474
adam-weiler <adam.weiler@yahoo.ca>
2019-08-17 10:53:47 -0400

    Adding pacman-switch.js

---
+++ pacman-switch.js
@@ -0,0 +1,19 @@
+const ghost = 'Blinky';
+let colour;
+
+switch (ghost) {
+    case 'Inky':
+        colour = 'Cyan';
+        break;
+    case 'Blinky':
+        colour = 'Red';
+        break;
+    case 'Pinky':
+        colour = 'Pink';
+        break;
+    case 'Clyde':
+        colour = 'Orange';
+        break;
+}
+
+console.log(`${ghost} is the colour ${colour}`);
