3fc00c53228b08f5168f9a272984d35f9dccd563
adam-weiler <adam.weiler@yahoo.ca>
2019-08-17 10:50:54 -0400

    Modifying pacman-objects-2.js, adding pacman-if-else.js, and renaming to pacman-for-loops.js

--- pacman-objects-2.js
+++ pacman-objects-2.js
@@ -1,12 +1,12 @@
 const ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
 console.log(ghosts);
 
-console.log(ghosts.inky);
-console.log(ghosts.blinky);
+console.log(ghosts.inky); //Cyan
+console.log(ghosts.blinky); //Red
 
-console.log(ghosts['inky']);
-console.log(ghosts['blinky']);
+console.log(ghosts['inky']);  //Cyan
+console.log(ghosts['blinky']); //Red
 
 
 const ghostName = 'clyde';
-console.log(ghosts[ghostName]);
+console.log(ghosts[ghostName]); //Orange
